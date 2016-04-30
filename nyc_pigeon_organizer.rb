bdc25b439f1699e69cc81ad66399266aed79a25f
Gilad Barkan <giladbarkan@live.com>
2016-04-30 06:44:19 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  result = {}
+
+  data.each do |attr, attr_types|
+    attr_types.each do |type, names|
+      names.each do |name|
+        if result.has_key?(name)
+          if result[name].has_key?(attr)
+            result[name][attr] << type.to_s
+          else
+          	result[name][attr] = [type.to_s]
+          end
+        else
+          result[name] = {attr => [type.to_s]}
+        end
+      end
+    end
+  end
+
+  result
 end
